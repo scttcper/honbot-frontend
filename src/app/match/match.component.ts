a620faf269892b83f9de9cdb077b8ca028c6afbb
Scott Cooper <scttcper@gmail.com>
2017-01-20 20:05:18 -0800

    format match

--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -5,17 +5,35 @@ import * as _ from 'lodash';
 import { Api } from '../api';
 import { getLobby } from '../util';
 
+const needsTotal = [
+  'kills',
+  'deaths',
+  'assists',
+  'cs',
+  'denies',
+  'gpm',
+  'apm',
+  'xpm',
+  'wards',
+  'herodmg',
+  'bdmg',
+  'level',
+];
+
 @Component({
   selector: 'hb-match',
   templateUrl: './match.component.html',
   styleUrls: ['./match.component.scss']
 })
 export class MatchComponent implements OnInit {
-  match: any;
+  match: any = {};
   matchId: string;
   team1: any;
   team2: any;
   lobby: string;
+  teamNames = ['Legion', 'Hellbourne'];
+  winner: number;
+  teamTotals: any = [{}, {}];
 
   constructor(
     private route: ActivatedRoute,
@@ -30,11 +48,22 @@ export class MatchComponent implements OnInit {
   }
   setupMatch(match: any) {
     this.match = match;
+    this.match.duration = new Date(match.length * 1000).toISOString().substr(11, 8);
     this.match.players = this.match.players.sort((a, b) => a.position - b.position);
     const g = _.groupBy(this.match.players, _.property('team'));
     this.team1 = g[1];
     this.team2 = g[2];
     this.lobby = getLobby(match);
+    for (const p of match.players) {
+      for (const v of needsTotal) {
+        if (!_.has(this.teamTotals[p.team - 1], v)) {
+          this.teamTotals[p.team - 1][v] = 0;
+        }
+        this.teamTotals[p.team - 1][v] += p[v];
+      }
+    }
+    this.winner = this.teamTotals[0].wins > this.teamTotals[1].wins ? 0 : 1;
   }
+
 
 }
