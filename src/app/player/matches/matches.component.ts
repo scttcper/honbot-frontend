841ac23c74b3be1b66a3829aa9bb1bca417a8c4a
Scott Cooper <scttcper@gmail.com>
2017-06-11 22:49:22 -0700

    allow filtering of matches

--- src/app/player/matches/matches.component.ts
+++ src/app/player/matches/matches.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { PaginationInstance } from 'ngx-pagination';
 import * as _ from 'lodash';
 
+import { subWeeks, subMonths, subYears, isAfter } from 'date-fns';
 import { Api } from '../../api';
 
 @Component({
@@ -13,7 +14,14 @@ import { Api } from '../../api';
 export class MatchesComponent implements OnInit {
   loading = false;
   matches: any[];
+  unfiltered: any[];
   maxLength = 0;
+
+  time = '';
+  mode = '';
+  lobby = '';
+  team = '';
+  length = '';
 
   config: PaginationInstance = {
     id: 'matches',
@@ -31,11 +39,58 @@ export class MatchesComponent implements OnInit {
       this.api
         .getPlayerMatches(params['nickname'])
         .subscribe((res) => {
-          this.matches = res.matches;
+          this.unfiltered = res.matches;
           const max = _.maxBy(this.matches, _.property('length')) || {};
           this.maxLength = max.length || 0;
+          this.applyFilters();
         });
     });
+  }
+  applyFilters() {
+    const date = this.selectedDate();
+    this.matches = _.filter(this.unfiltered, (n) => {
+      return this.filterTime(n, date) &&
+        this.filterMode(n) &&
+        this.filterTeam(n) &&
+        this.filterLobby(n);
+    });
+  }
+  selectedDate() {
+    switch (this.time) {
+      case 'week':
+        return subWeeks(new Date(), 1);
+      case 'month':
+        return subMonths(new Date(), 1);
+      case '3month':
+        return subMonths(new Date(), 3);
+      case '6month':
+        return subMonths(new Date(), 6);
+      case 'year':
+        return subYears(new Date(), 1);
+    }
+    return new Date(0);
+  }
+  filterTime(match, date) {
+    return new Date(match.date) >= date;
+  }
+  filterMode(match) {
+    if (!this.mode || this.mode === 'all') {
+      return true;
+    }
+    return this.mode === match.mode;
+  }
+  filterLobby(match) {
+    if (!this.lobby || this.lobby === 'all') {
+      return true;
+    }
+    return this.lobby === match.type;
+  }
+  filterTeam(match) {
+    if (!this.team || this.team === 'all') {
+      return true;
+    }
+    const t = this.team === 'legion' ? 1 : 2;
+    return match.team === t;
   }
 
 }
